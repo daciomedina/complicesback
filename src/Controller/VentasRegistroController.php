232a04c833f1ed155844f3d3e11fc65b9f4eda1f
Dacio <dmedina.ve@gmail.com>
2021-10-03 09:56:07 +0200

    add try catch

--- src/Controller/VentasRegistroController.php
+++ src/Controller/VentasRegistroController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 /**
  * Class VentasRegistroSiteController
@@ -26,49 +27,85 @@ class VentasRegistroController extends AbstractController
     }
 
     /**
-     * @Route("/api/ventasregistro/", name="add_ventasregistro", methods={"POST"})
+     * @Route("/api/ventasregistros/", name="add_ventasRegistros", methods={"POST"})
      */
     public function addVentasRegistro(Request $request): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        try{
+            $data = json_decode($request->getContent(), true);
 
-        $clientName = $data['clientName'];
-        $dateAdd =$data['dateAdd'];
-        $friendName = $data['friendName'];
-        $deliveryAddress = $data['deliveryAddress'];
-        $deliveryDate = $data['deliveryDate'];
-        $ammount = $data['ammount'];
-        $phoneNumber = $data['phoneNumber'];
-        $status = $data['status'];
+            $clientName = $data['clientName'];
+            $dateAdd =$data['dateAdd'];
+            $friendName = $data['friendName'];
+            $deliveryAddress = $data['deliveryAddress'];
+            $deliveryDate = $data['deliveryDate'];
+            $ammount = $data['ammount'];
+            $phoneNumber = $data['phoneNumber'];
+            $status = $data['status'];
 
-        if (empty($clientName) || empty($friendName) || empty($deliveryAddress) || empty($phoneNumber)) {
-            throw new NotFoundHttpException('Expecting mandatory parameters!');
+            if (empty($clientName) || empty($friendName) || empty($deliveryAddress) || empty($phoneNumber)) {
+                throw new NotFoundHttpException('Expecting mandatory parameters!');
+            }
+
+            $this->ventasRegistroRepository->saveVentasRegistro($clientName, $dateAdd, $friendName, $deliveryAddress, $deliveryDate, $ammount, $phoneNumber, $status);
+        }catch(Throwable $exception){
+            return new JsonResponse(['status' => 'Error en los datos -- '.$exception->getMessage()], Response::HTTP_BAD_REQUEST);
         }
-
-        $this->ventasRegistroRepository->saveVentasRegistro($clientName, $dateAdd, $friendName, $deliveryAddress, $deliveryDate, $ammount, $phoneNumber, $status);
-
         return new JsonResponse(['status' => 'VentasRegistro created!'], Response::HTTP_CREATED);
     }
 
     /**
-     * @Route("/api/ventasregistro/{id}", name="get_one_ventasregistro", methods={"GET"})
+     * @Route("/api/ventasregistros/{id}", name="get_one_ventasRegistros", methods={"GET"})
      */
     public function getOneVentasRegistro($id): JsonResponse
     {
-        $ventasregistro = $this->ventasRegistroRepository->findOneBy(['id' => $id]);
+        try{
+            $ventasRegistros = $this->ventasRegistroRepository->findOneBy(['id' => $id]);
 
-        $data = [
-            'id' => $ventasregistro->getId(),
-            'clientName' => $ventasregistro->getClientName(),
-            'dateAdd' => $ventasregistro->getDateAdd(),
-            'friendName' => $ventasregistro->getfriendName(),
-            'deliveryAddress' => $ventasregistro->getDeliveryAddress(),
-            'deliveryDate' => $ventasregistro->getDeliveryDate(),
-            'ammount' => $ventasregistro->getAmmount(),
-            'phoneNumber' => $ventasregistro->getPhoneNumber(),
-            'status' => $ventasregistro->getStatus(),
-        ];
-
+            $data = [
+                'id' => $ventasRegistros->getId(),
+                'clientName' => $ventasRegistros->getClientName(),
+                'dateAdd' => $ventasRegistros->getDateAdd(),
+                'friendName' => $ventasRegistros->getfriendName(),
+                'deliveryAddress' => $ventasRegistros->getDeliveryAddress(),
+                'deliveryDate' => $ventasRegistros->getDeliveryDate(),
+                'ammount' => $ventasRegistros->getAmmount(),
+                'phoneNumber' => $ventasRegistros->getPhoneNumber(),
+                'status' => $ventasRegistros->getStatus(),
+            ];
+        }catch(Throwable $exception){
+            return new JsonResponse(['status' => 'Error recuperando datos -- '.$exception->getMessage()], Response::HTTP_NO_CONTENT);    
+        }
         return new JsonResponse($data, Response::HTTP_OK);
     }
+
+    /**
+     * @Route("/api/ventasregistros/", name="get_all_ventasRegistros", methods={"GET"})
+     */
+    public function getAll(): JsonResponse
+    {
+        try{
+            $ventasRegistros = $this->ventasRegistroRepository->findAll();
+            $data = [];
+
+            foreach ($ventasRegistros as $ventasRegistro) {
+                $data[] = [
+                    'id' => $ventasRegistro->getId(),
+                    'clientName' => $ventasRegistro->getClientName(),
+                    'dateAdd' => $ventasRegistro->getDateAdd(),
+                    'friendName' => $ventasRegistro->getfriendName(),
+                    'deliveryAddress' => $ventasRegistro->getDeliveryAddress(),
+                    'deliveryDate' => $ventasRegistro->getDeliveryDate(),
+                    'ammount' => $ventasRegistro->getAmmount(),
+                    'phoneNumber' => $ventasRegistro->getPhoneNumber(),
+                    'status' => $ventasRegistro->getStatus(),
+                ];
+            }
+        }catch(Throwable $exception){
+            return new JsonResponse(['status' => 'Error recuperando datos -- '.$exception->getMessage()], Response::HTTP_NO_CONTENT);    
+        }
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    
 }
